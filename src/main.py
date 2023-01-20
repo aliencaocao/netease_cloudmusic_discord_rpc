0672f57abac1f6b4ecdb9efe3785be19da69f0de
Leko <leko@leko.moe>
2023-01-20 21:52:04 +0800

    feat: allow fetching song info from local

--- src/main.py
+++ src/main.py
@@ -11,6 +11,8 @@ from pyMeow import open_process, close_process, get_module, r_float64, r_bytes, 
 from typing import Tuple
 import urllib.request
 import re
+from os import path
+import json
 
 __version__ = '0.2.0'
 supported_cloudmusic_version = '2.10.6.3993'
@@ -86,32 +88,63 @@ first_run = True
 
 song_info_cache = {}
 def get_song_info_from_netease(song_id: str) -> str:
-    song_info = {}
-    url = f'https://music.163.com/song?id={song_id}'
-    with urllib.request.urlopen(url) as response:
-        html = response.read().decode('utf-8')
+    try:
+        song_info = {}
+        url = f'https://music.163.com/song?id={song_id}'
+        with urllib.request.urlopen(url) as response:
+            html = response.read().decode('utf-8')
 
-        re_img = re.compile(r'<meta property="og:image" content="(.+)"')
-        song_info["cover"] = re_img.findall(html)[0]
+            re_img = re.compile(r'<meta property="og:image" content="(.+)"')
+            song_info["cover"] = re_img.findall(html)[0]
 
-        re_album = re.compile(r'<meta property="og:music:album" content="(.+)"')
-        song_info["album"] = re_album.findall(html)[0]
+            re_album = re.compile(r'<meta property="og:music:album" content="(.+)"')
+            song_info["album"] = re_album.findall(html)[0]
 
-        re_duration = re.compile(r'<meta property="music:duration" content="(.+)"')
-        song_info["duration"] = int(re_duration.findall(html)[0])
+            re_duration = re.compile(r'<meta property="music:duration" content="(.+)"')
+            song_info["duration"] = int(re_duration.findall(html)[0])
 
-        re_artist = re.compile(r'<meta property="og:music:artist" content="(.+)"')
-        song_info["artist"] = re_artist.findall(html)[0]
+            re_artist = re.compile(r'<meta property="og:music:artist" content="(.+)"')
+            song_info["artist"] = re_artist.findall(html)[0]
 
-        re_title = re.compile(r'<meta property="og:title" content="(.+)"')
-        song_info["title"] = re_title.findall(html)[0]
+            re_title = re.compile(r'<meta property="og:title" content="(.+)"')
+            song_info["title"] = re_title.findall(html)[0]
 
-        song_info_cache[song_id] = song_info
+            song_info_cache[song_id] = song_info
+        return True
+    except:
+        print(f"Error while reading from remote: {song_id}")
+        return False
+
+def get_song_info_from_local(song_id: str) -> str:
+    filepath = path.join(path.expandvars('%LOCALAPPDATA%'), 'Netease/CloudMusic/webdata/file/history')
+    if not path.exists(filepath):
+        return False
+    try:
+        with(open(filepath, 'r', encoding='utf-8')) as f:
+            history = json.load(f)
+            song_info_raw = next(x for x in history if str(x['track']['id']) == song_id)
+            if not song_info_raw:
+                return False
+            song_info = {
+                'cover': song_info_raw['track']['album']['picUrl'],
+                'album': song_info_raw['track']['album']['name'],
+                'duration': song_info_raw['track']['duration'] / 1000,
+                'artist': '/'.join([x['name'] for x in song_info_raw['track']['artists']]),
+                'title': song_info_raw['track']['name'],
+            }
+
+            song_info_cache[song_id] = song_info
+        return True
+    except:
+        print(f"Error while reading from local history file: {song_id}")
+        return False
+
 
 def get_song_info(song_id: str) -> str:
     global song_info_cache
     if song_id not in song_info_cache:
-        get_song_info_from_netease(song_id)
+        if not get_song_info_from_local(song_id):
+            get_song_info_from_netease(song_id)
     return song_info_cache[song_id]
 
 def update():
@@ -144,6 +177,11 @@ def update():
 
     songid_array = r_uint(process, module_base + song_array_offset)
     song_id = r_bytes(process, songid_array, 0x14).decode('utf-16')
+
+    re_song_id = re.compile(r'(\d+)')
+    if not re_song_id.match(song_id):
+        # Song ID is not ready yet.
+        return
 
     song_info = get_song_info(song_id)
 
